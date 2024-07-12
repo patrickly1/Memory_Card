4ebadda5e9d69ea2add3a8a233fa168dc26a158f
Patrick Ly <112744444447551+patrickly1@users.noreply.github.com>
2024-07-12 03:17:17 -0400

    Added 12 buttons, with 16 total buttons that will be randomly chosen upon each render. Clicking a button also re-renders the page. If a duplicate button is clicked, the user will be notified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,79 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Card from './Card'
 
 function App() {
+  const pokemonCards = [
+    { pic: "1", title: "1"},
+    { pic: "2", title: "2"},
+    { pic: "3", title: "3"},
+    { pic: "4", title: "4"},
+    { pic: "5", title: "5"},
+    { pic: "6", title: "6"},
+    { pic: "7", title: "7"},
+    { pic: "8", title: "8"},
+    { pic: "9", title: "9"},
+    { pic: "10", title: "10"},
+    { pic: "11", title: "11"},
+    { pic: "12", title: "12"},
+    { pic: "13", title: "13"},
+    { pic: "14", title: "14"},
+    { pic: "15", title: "15"},
+    { pic: "16", title: "16"},
+  ]
+
+  const [shuffledPokemonCards, setShuffledPokemonCards] = useState([]);
+  const [shuffleTrigger, setShuffleTrigger] = useState(0);
+  const [seenPokemonCards, setSeenPokemonCards] = useState(new Set());
+  const [duplicatePokemonCard, setDuplicatePokemonCard] = useState(false);
+
+  useEffect(() => {
+    //shuffle the array of pokemon cards
+    function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+
+      return array;
+    }
+
+    const shuffledPokemonCards = shuffleArray([...pokemonCards]).slice(0, 12);
+    setShuffledPokemonCards(shuffledPokemonCards);
+  }, [shuffleTrigger]); // dependency on shuffleTrigger
+
+  const handleCardClick = (index) => {
+    const card = shuffledPokemonCards[index];
+    if (seenPokemonCards.has(card.title)) {
+      setDuplicatePokemonCard(true);
+    } else{
+      const newSet = new Set(seenPokemonCards);
+      newSet.add(card.title);
+      setSeenPokemonCards(newSet);
+      setShuffleTrigger(prev => prev + 1);
+    }
+  };
 
   return (
-    <Card>
-      "Hello"
-    </Card>
+    <div className="container">
+      <div className="topContainer">
+        <h2>
+        Memory Card Game  
+        </h2>
+        <div>Times Clicked {shuffleTrigger}</div>
+        <div>{duplicatePokemonCard && "Duplicate card clicked!"}</div>
+      </div>
+      <div className="bottomContainer">
+        {shuffledPokemonCards.map((pokemonCard, index) => (
+          <Card
+            key={index}
+            pic={pokemonCard.pic}
+            title={pokemonCard.title}
+            index={index}
+            handleCardClick={handleCardClick}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react';
-
-export default function Card() {
-    
+export default function Card({ pic, 
+    title ,
+    index,
+    handleCardClick
+}) {
     return (
-        <>
-
-        </>
+        <div className="cardContainer">
+            <button onClick={() => handleCardClick(index)}>
+                <div className="buttonPic">
+                    buttonPic {pic}
+                </div>
+                <div className="buttonTitle">
+                    buttonTitle {title}
+                </div>
+            </button>
+        </div>
     )
 }
